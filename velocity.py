4c1b8d5c5862b8be2dda550eab0de702321f2525
RaulS22 <rau1d3as@gmail.com>
2026-04-30 20:00:11 -0300

    new: tests for p pealks and velocity

--- velocity.py
+++ velocity.py
@@ -1,6 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from obspy import read, read_inventory
+from obspy import read, read_inventory, UTCDateTime
 from pathlib import Path
-from concurrent.futures import ProcessPoolExecutor
 import gc
+
+# =========================
+# Paths
+# =========================
+input_dir = Path("/home/rauls/Desktop/VirgoBRET/SENA-mseed")
+output_dir = Path("sena-jan24-velocity")
+output_dir.mkdir(exist_ok=True)
+
+inv = read_inventory("fdsn_station.xml")
+
