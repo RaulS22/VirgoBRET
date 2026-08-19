50e93b1eb12edf67bab41880696a23f8ddefc698
RaulS22 <rau1d3as@gmail.com>
2026-08-19 15:38:08 -0300

    new: MDS test

---
+++ MDS.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.manifold import MDS
+
+DATA_DIR = Path("/home/rauls/Desktop/GithubITA/VirgoBRET/cut_parquet_data")
+PARQUET_FILES = sorted(DATA_DIR.glob("**/*/qtransform_features.parquet"))
+MONTH_ORDER = {"jan": 1, "feb": 2, "mar": 3, "apr": 4, "may": 5, "jun": 6, "jul": 7, "ago": 8, "sep": 9, "oct": 10, "nov": 11, "dec": 12}
+RANDOM_STATE = 42
+
+if len(PARQUET_FILES) == 0:
+    raise FileNotFoundError(
+        f"No parquet files found inside: {DATA_DIR}")
+
+print(f"Found {len(PARQUET_FILES)} parquet files.")
+
+dataframes = []
+
+parquet_files = list(DATA_DIR.glob("*/*/qtransform_features.parquet"))
+parquet_files.sort(key=lambda p: (int(p.parent.parent.name), MONTH_ORDER[p.parent.name]))
+
+for file in parquet_files:
+    print(f"Loading: {file}")
+
+    df = pd.read_parquet(file)
+    folder = file.parent
+
+    image_paths = [str(folder / f"qtransform_{i:06d}.pdf")for i in range(len(df))]
+    missing_images = [path for path in image_paths if not Path(path).exists()]
+
+    if missing_images:
+        raise FileNotFoundError(f"{len(missing_images)} PDF files are missing in {folder}. First missing file: {missing_images[0]}")
+
+    df["image_path"] = image_paths
+    df["source_file"] = file.name
+    df["source_path"] = str(file)
+
+    dataframes.append(df)
+
+df_all = pd.concat(dataframes,ignore_index=True)
+
+feature_columns = [col for col in df_all.columns if col.startswith("feature_")]
+if len(feature_columns) == 0:
+    raise ValueError("No feature_* columns found.")
+
+print(f"Number of features: {len(feature_columns)}")
+
+X = df_all[feature_columns].to_numpy(dtype=np.float64)
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+pca = PCA(n_components=40, random_state=RANDOM_STATE)
+X_pca = pca.fit_transform(X_scaled)
+
+mds = MDS(n_components=2,random_state=RANDOM_STATE)
+X_mds = mds.fit_transform(X_pca)
+
+fig, ax = plt.subplots(figsize=(12, 10))
+scatter = ax.scatter(X_mds[:, 0], X_mds[:, 1],s=5,alpha=0.5)
+ax.set_title(f"MDS Projection of dataset")
+ax.set_xlabel("MDS 1")
+ax.set_ylabel("MDS 2")
+
+plt.tight_layout()
+plt.show()
